3e8358317792069dfaa6d871d046b94a4aafdb50
delialo <yuu.miyamichi@gmail.com>
2025-12-21 12:57:16 +0100

    added rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project is open sourced on{" "}
-        <a href="https://github.com/delialo/react-weather-app" target="_blank">
+        <a
+          href="https://github.com/delialo/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           GitHub
         </a>
         and hosted on
-        <a href="" target="_blank">
+        <a href="#" target="_blank" rel="noreferrer">
           Netlify
         </a>
-        .<br />{" "}
+        .
       </footer>
     </div>
   );
