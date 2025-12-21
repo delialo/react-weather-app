b48a977a9c0b92e724cd4d6a4d30a55e3a80d983
delialo <yuu.miyamichi@gmail.com>
2025-12-21 14:18:45 +0100

    added bootstrap, cleared css

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is open sourced on{" "}
-        <a
-          href="https://github.com/delialo/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        {/* and hosted on
+      <div className="container mt-3 mb-3">
+        <h1>Weather App</h1>
+        <footer>
+          This project is open sourced on{" "}
+          <a
+            href="https://github.com/delialo/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          {/* and hosted on
         <a href="#" target="_blank" rel="noreferrer">
           Netlify
         </a> */}
-        .
-      </footer>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
