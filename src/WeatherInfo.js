848880e28963f7b66279a8936f2b590a898e6e95
delialo <yuu.miyamichi@gmail.com>
2026-05-05 13:41:07 +0200

    Adjusting responsiveness on small screen

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,26 +1,27 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import "./WeatherInfo.css";
 
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
-      <ul>
+      <ul className="weather-description">
         <li>
           <FormattedDate date={props.data.date} />
         </li>
         <li>{props.data.description}</li>
       </ul>
       <div className="row">
-        <div className="col-6 d-flex align-items-center">
+        <div className="col-sm-6 d-flex align-items-center">
           <img src={props.data.icon} alt={props.data.iconDescription} />
           <span className="temperature">
             {Math.round(props.data.temperature)}
           </span>
           <span className="unit ">°C</span>
         </div>
-        <div className="col-6">
-          <ul>
+        <div className="col-sm-6 d-flex align-items-center mb-3">
+          <ul className="weather-details">
             <li>Feels like: {Math.round(props.data.feelsLike)}°C</li>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {Math.round(props.data.wind)} km/h</li>
